65c51b9c3011a3d93f1986be5cfac9c9c03800ea
Zelly <43312573+liu2-3zhi@users.noreply.github.com>
2024-09-23 23:03:38 +0800

    Update Download_jdk21.sh

--- Phone/Download_jdk21.sh
+++ Phone/Download_jdk21.sh
@@ -6,16 +6,25 @@ URL="https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz"
 # 设置本地文件名
 FILE_NAME="jdk-21_linux-x64_bin.tar.gz"
 
-# 设置解压后的文件夹名称
-FOLDER_NAME="jdk-21_linux-x64"
+# 设置目标文件夹
+TARGET_FOLDER="./Direct/jdk-21_linux-x64"
+
+# 设置下载文件夹
+DOWNLOAD_FOLDER="./Direct"
+
+# 创建下载文件夹
+if [ ! -d "$DOWNLOAD_FOLDER" ]; then
+    echo "创建下载文件夹：$DOWNLOAD_FOLDER"
+    mkdir -p "$DOWNLOAD_FOLDER"
+fi
 
 # 检查文件是否已存在
-if [ -f "$FILE_NAME" ]; then
-    echo "文件 $FILE_NAME 已存在，跳过下载。"
+if [ -f "$DOWNLOAD_FOLDER/$FILE_NAME" ]; then
+    echo "文件 $DOWNLOAD_FOLDER/$FILE_NAME 已存在，跳过下载。"
 else
     # 下载文件
     echo "开始下载文件..."
-    wget -c $URL -O $FILE_NAME
+    wget -c $URL -O "$DOWNLOAD_FOLDER/$FILE_NAME"
     if [ $? -eq 0 ]; then
         echo "下载完成。"
     else
@@ -25,12 +34,17 @@ else
 fi
 
 # 创建目标文件夹
-if [ -d "$FOLDER_NAME" ]; then
-    echo "文件夹 $FOLDER_NAME 已存在，跳过解压。"
+if [ ! -d "$TARGET_FOLDER" ]; then
+    echo "创建目标文件夹：$TARGET_FOLDER"
+    mkdir -p "$TARGET_FOLDER"
+fi
+
+# 解压文件
+if [ -d "$TARGET_FOLDER" ]; then
+    echo "文件夹 $TARGET_FOLDER 已存在，跳过解压。"
 else
-    # 解压文件
     echo "开始解压文件..."
-    tar -zxvf $FILE_NAME
+    tar -zxvf "$DOWNLOAD_FOLDER/$FILE_NAME" -C "$TARGET_FOLDER"
     if [ $? -eq 0 ]; then
         echo "解压完成。"
     else
