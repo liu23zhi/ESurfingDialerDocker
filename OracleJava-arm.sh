6a7be753c8da4839d91b826eabee43c7e2bb410c
Zelly <43312573+liu2-3zhi@users.noreply.github.com>
2024-10-11 08:21:10 +0800

    Update OracleJava-arm.sh

--- OracleJava-arm.sh
+++ OracleJava-arm.sh
@@ -3,5 +3,5 @@
 chmod +x ./Oracle-Docker-images/OracleJava/21/build.sh
 cd ./Oracle-Docker-images/OracleJava/21/
 #docker build --file Dockerfile.ol9 --tag oracle/jdk:21-ol9 .
-docker buildx build --platform linux/amd64 --file Dockerfile.ol9 --tag oracle/jdk:21-ol9  --load .
+docker buildx build --platform linux/arm64 --file Dockerfile.ol9 --tag oracle/jdk:21-ol9  --load .
 #./build.sh 9
