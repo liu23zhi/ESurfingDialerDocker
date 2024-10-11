1b5ead7797b7e7464eba6fc8e3960d64a33bfa11
Zelly <43312573+liu2-3zhi@users.noreply.github.com>
2024-10-11 08:20:43 +0800

    Update build-arm.sh

--- Phone/build-arm.sh
+++ Phone/build-arm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #cd./Phone
 #docker build -t esurfingdockerphone .
-docker buildx build --platform linux/amrm4 --tag esurfingdockerphone --load .
+docker buildx build --platform linux/arm64 --tag esurfingdockerphone --load .
 #docker save esurfingdockerphone -o./ESurfingDockerPhone.tar.gz
