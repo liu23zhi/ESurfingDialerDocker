5c3498e96cbea52121d175de0154425c3ce5c77a
Zelly <43312573+liu2-3zhi@users.noreply.github.com>
2024-10-15 23:45:56 +0800

    小更新

--- PC/build-arm.sh
+++ PC/build-arm.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #cd./Phone
 #docker build -t esurfingdockerpc .
+mkdir -p ./ubuntu-base-24.04.1-base-amd64/app/
+#cp ./run 
 docker buildx build --platform linux/arm64 --tag esurfingdockerpc --load .
 #docker save esurfingdockerphone -o./ESurfingDockerPhone.tar.gz
