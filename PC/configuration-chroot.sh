a2fe10b413219c13fb91bdc2bf947bedd37bad83
Zelly <43312573+liu2-3zhi@users.noreply.github.com>
2024-10-19 10:47:13 +0800

    又没有权限？

--- PC/configuration-chroot.sh
+++ PC/configuration-chroot.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 # 挂载 /etc/resolv.conf 为只读模式
-mount --bind /etc/resolv.conf /app/ubuntu-base/etc/resolv.conf
-dmesg | tail
-mount -o bind,ro /etc/resolv.conf /app/ubuntu-base/etc/resolv.conf
-dmesg | tail
+sudo mount --bind /etc/resolv.conf /app/ubuntu-base/etc/resolv.conf
+sudo dmesg | tail
+sudo mount -o bind,ro /etc/resolv.conf /app/ubuntu-base/etc/resolv.conf
+sudo dmesg | tail
 
 # 挂载 proc 为只读模式
-mount -t proc -o ro proc /app/ubuntu-base/proc
-dmesg | tail
+sudo mount -t proc -o ro proc /app/ubuntu-base/proc
+sudo dmesg | tail
 
 # 挂载本地 /dev/pts 到虚拟环境
 mkdir -p /app/ubuntu-base/dev/pts
-mount -t devpts devpts /app/ubuntu-base/dev/pts
-dmesg | tail
+sudo mount -t devpts devpts /app/ubuntu-base/dev/pts
+sudo dmesg | tail
 
 # 补全虚拟环境
 if [ ! -d /app/ubuntu-base/tmp ]; then
     mkdir /app/ubuntu-base/tmp
 fi
-chmod -R 777 /app/ubuntu-base/tmp
+sudo chmod -R 777 /app/ubuntu-base/tmp
