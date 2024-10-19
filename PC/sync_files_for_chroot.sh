bd68915ba43f524c32d1f2661691253ac65fca7c
Zelly <43312573+liu2-3zhi@users.noreply.github.com>
2024-10-19 13:42:04 +0800

    Update sync_files_for_chroot.sh

--- PC/sync_files_for_chroot.sh
+++ PC/sync_files_for_chroot.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # 创建目标目录（如果不存在）
-mkdir -p /app/proc/net
+mkdir -p /app/ubuntu-base/proc/net
 
 # 定期检查和同步文件
 while true; do
     # 检查并创建目标文件（如果不存在）
-    if [ ! -f /app/proc/net/route ]; then
-        touch /app/proc/net/route
+    if [ ! -f /app/ubuntu-base/proc/net/route ]; then
+        touch /app/ubuntu-base/proc/net/route
     fi
-    if [ ! -f /app/proc/net/ipv6_route ]; then
-        touch /app/proc/net/ipv6_route
+    if [ ! -f /app/ubuntu-base/proc/net/ipv6_route ]; then
+        touch /app/ubuntu-base/proc/net/ipv6_route
     fi
-    if [ ! -f /app/etc/resolv.conf ]; then
-        mkdir -p /app/etc
-        touch /app/etc/resolv.conf
-    fi
+    # if [ ! -f /app/ubuntu-base/etc/resolv.conf ]; then
+    #     mkdir -p /app/ubuntu-base/etc
+    #     touch /app/ubuntu-base/etc/resolv.conf
+    # fi
 
     # 同步文件内容
-    cp /proc/net/route /app/proc/net/route
-    cp /proc/net/ipv6_route /app/proc/net/ipv6_route
-    cp /etc/resolv.conf /app/etc/resolv.conf
+    cp /proc/net/route /app/ubuntu-base/proc/net/route
+    cp /proc/net/ipv6_route /app/ubuntu-base/proc/net/ipv6_route
+    cp /etc/resolv.conf /app/ubuntu-base/etc/resolv.conf
 
     sleep 1
 done
