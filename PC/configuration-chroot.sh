efd10a289f839d23bad7c2fc2f4a10b65d5c9c08
Zelly <43312573+liu2-3zhi@users.noreply.github.com>
2024-11-15 22:16:31 +0800

    添加环境变量"sleep"用于永远睡眠，便于调试。

--- PC/configuration-chroot.sh
+++ PC/configuration-chroot.sh
@@ -153,7 +153,17 @@ sudo chmod -R 777 /app/ubuntu-base/app/env_vars.sh
 # ls -l /app/ubuntu-base/app/ESurfingDialerClient/
 sudo chmod -R 777 /app/ubuntu-base/app/ESurfingDialerClient/run.sh
 
-#sleep infinity
+
+if [ $sleep ]; then
+    echo "当前环境变量\"sleep\" 为 \"$sleep\""
+    echo "环境变量\"sleep\"为\"true\"才能进入手动模式。"
+    if [ "$sleep" == "true" ]; then
+        echo "进入手动调试模式。"
+        echo "执行指令：sleep infinity"
+        sleep infinity
+    fi
+fi
+
 
 if test "$1" = "true"; then
 
