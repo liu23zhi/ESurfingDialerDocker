206f551db0525bc25abd348f3ef8b44b1a43b3e1
BadGhost520 <122501666+BadGhost520@users.noreply.github.com>
2024-11-25 21:43:30 +0800

    Update run.sh

--- Phone/run.sh
+++ Phone/run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+echo "Asia/Shanghai" > /etc/timezone
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 nohup ./host.sh &
 java -jar client.jar -u ${DIALER_USER} -p ${DIALER_PASSWORD}
