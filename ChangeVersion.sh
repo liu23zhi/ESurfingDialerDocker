af9724e9631569a990d5be06b13321700ad0f377
BadGhost520 <122501666+BadGhost520@users.noreply.github.com>
2024-12-08 11:31:44 +0800

    Update ChangeVersion.sh

--- ChangeVersion.sh
+++ ChangeVersion.sh
@@ -1,2 +1,10 @@
+#!/bin/sh
+
+# 更新 package.json 中的版本号，但不生成 git 标签
+#npm version major -no-git-tag-version
+#npm version minor -no-git-tag-version
 npm version patch -no-git-tag-version
-echo "Version=$(npm view . version)" >> $GITHUB_ENV
+
+# 将版本号输出到环境变量
+VERSION=$(npm view . version)
+echo "VERSION=$VERSION" >> $GITHUB_ENV
