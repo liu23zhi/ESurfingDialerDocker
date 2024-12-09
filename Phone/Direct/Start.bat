9554ff56749048dd88a765b8b0dab35fb36615c5
BadGhost520 <122501666+BadGhost520@users.noreply.github.com>
2024-12-09 12:33:06 +0800

    Update Start.bat

--- Phone/Direct/Start.bat
+++ Phone/Direct/Start.bat
@@ -1,62 +1,62 @@
 @echo off
 setlocal enabledelayedexpansion
 
-::����ȴ�ʱ��
+::输入等待时间
 set "TIMEOUT_Print=1"
 
-:: ���ó�ʱʱ�䣨�룩
+:: 设置超时时间（秒）
 set TIMEOUT=5
 
-:: ���õ���ģʽ
+:: 设置调试模式
 set "DEBUG=true"
 
-:: �ļ��������
+:: 文件完整情况
 set "COMPLETENESS=true"
 
-:: ��ȡ�ű�����Ŀ¼�ľ��Ե�ַ
+:: 获取脚本所在目录的绝对地址
 set "SCRIPT_DIR=%~dp0%"
 
-:: ����Java Home·��
-set "JAVA_HOME=%SCRIPT_DIR%jdk_windows-x64"
+:: 设置Java Home路径
+set "JAVA_HOME=%SCRIPT_DIR%jdk-windows"
 
-:: ����Java·��
+:: 设置Java路径
 set "JAVA_PATH=%JAVA_HOME%\bin\java.exe"
 
-:: ����Jar�ļ�·��
+:: 设置Jar文件路径
 set "JAR_FILE=%SCRIPT_DIR%client.jar"
 
-:: ���������ļ�·��
+:: 设置配置文件路径
 set "CONFIG_FILE=%SCRIPT_DIR%ESurfingDialerAccount.config"
 
 set "TIMEOUT_Print_Command=timeout /t %TIMEOUT_Print% /nobreak >nul"
 
-:: ����ļ��Ƿ�����
+:: 检查文件是否完整
 if not exist "%JAVA_PATH%" (
     set "COMPLETENESS=false"
-    echo Javaִ���ļ������ڡ��ļ�ȱʧ��
-    echo ���������ء�
+    echo Java执行文件不存在。文件缺失。
+    echo 请重新下载。
     %TIMEOUT_Print_Command%
 )
 
 if not exist "%JAR_FILE%" (
     set "COMPLETENESS=false"
-    echo Jar�ļ������ڡ��ļ�ȱʧ��
-    echo ���������ء�
+    echo Jar文件不存在。文件缺失。
+    echo 请重新下载。
     %TIMEOUT_Print_Command%
 )
 
 if "%COMPLETENESS%"=="false" (
-    echo �ļ����������޷�����ִ�С�
+    echo 文件不完整，无法继续执行。
     %TIMEOUT_Print_Command%
     goto END
 )
 
 
 
-:: ����Java��������
+:: 设置Java环境变量
 set "PATH=%JAVA_HOME%\bin;%PATH%"
 
-:: ��ȡ�����ļ�����
+:: 读取配置文件内容
 if exist "%CONFIG_FILE%" (
     for /f "tokens=1,* delims==" %%a in ('type "%CONFIG_FILE%"') do (
         set "value=%%b"
@@ -79,17 +79,17 @@ goto Check_Config
     set USE_CONFIG=""
     set NO_PROMPT=""
     if "%DEBUG%"=="true" (
-        echo �����ļ������ڣ�ʹ�ÿ�����Ϊ����ֵ��
+        echo 配置文件不存在，使用空置作为参数值。
         %TIMEOUT_Print_Command%
     )
 )
 
 :Check_Config
 if "%DEBUG%"=="true" (
-    echo �����ļ����ڣ���ȡ�����û���Ϊ: %USERNAME%
-    echo �����ļ����ڣ���ȡ��������Ϊ: %PASSWORD%
-    echo �Ƿ�ʹ�������ļ�: %USE_CONFIG%
-    echo �Ƿ�����ʾ: %NO_PROMPT%
+    echo 配置文件存在，读取到的用户名为: %USERNAME%
+    echo 配置文件存在，读取到的密码为: %PASSWORD%
+    echo 是否使用配置文件: %USE_CONFIG%
+    echo 是否不再提示: %NO_PROMPT%
     %TIMEOUT_Print_Command%
 )
 
@@ -97,7 +97,7 @@ if "%DEBUG%"=="true" (
 if "%USE_CONFIG%"=="" (
     set USE_CONFIG=false
     if "%DEBUG%"=="true" (
-        echo "USE_CONFIG"�������Ϸ���������ΪĬ��ֵ"false"
+        echo "USE_CONFIG"参数不合法，已重置为默认值"false"
         %TIMEOUT_Print_Command%
     )
 ) else (
@@ -105,7 +105,7 @@ if "%USE_CONFIG%"=="" (
         if not "%USE_CONFIG%"=="true" (
             set USE_CONFIG=false
             if "%DEBUG%"=="true" (
-                echo "USE_CONFIG"�������Ϸ���������ΪĬ��ֵ"false"
+                echo "USE_CONFIG"参数不合法，已重置为默认值"false"
                 %TIMEOUT_Print_Command%
             )
         )
@@ -115,7 +115,7 @@ if "%USE_CONFIG%"=="" (
 if "%NO_PROMPT%"=="" (
     set NO_PROMPT=false
     if "%DEBUG%"=="true" (
-        echo "NO_PROMPT"�������Ϸ���������ΪĬ��ֵ"false"
+        echo "NO_PROMPT"参数不合法，已重置为默认值"false"
         %TIMEOUT_Print_Command%
     )
 ) else (
@@ -123,7 +123,7 @@ if "%NO_PROMPT%"=="" (
         if not "%NO_PROMPT%"=="true" (
             set USE_CONFIG=false
             if "%DEBUG%"=="true" (
-                echo "NO_PROMPT"�������Ϸ���������ΪĬ��ֵ"false"
+                echo "NO_PROMPT"参数不合法，已重置为默认值"false"
                 %TIMEOUT_Print_Command%
             )
         )
@@ -134,7 +134,7 @@ if "%USE_CONFIG%"=="true" (
     if "%USERNAME%" == "" (
         set USE_CONFIG_CHECK= false
         if "%DEBUG%"=="true" (
-            echo "USERNAME"���������ڣ��޷�����"USE_CONFIG"��"true"
+            echo "USERNAME"参数不存在，无法设置"USE_CONFIG"成"true"
             %TIMEOUT_Print_Command%
         )
     )
@@ -144,7 +144,7 @@ if "%USE_CONFIG%"=="true" (
     if "%PASSWORD%" == "" (
         set USE_CONFIG_CHECK= false
         if "%DEBUG%"=="true" (
-            echo "PASSWORD"���������ڣ��޷�����"USE_CONFIG"��"true"
+            echo "PASSWORD"参数不存在，无法设置"USE_CONFIG"成"true"
             %TIMEOUT_Print_Command%
         )
     )
@@ -153,7 +153,7 @@ if "%USE_CONFIG%"=="true" (
 if "%USE_CONFIG_CHECK%" == "false" (
     set USE_CONFIG=false
     if "%DEBUG%"=="true" (
-        echo �����ļ���鲻ͨ�����޷�����"USE_CONFIG"��"true"
+        echo 配置文件检查不通过，无法设置"USE_CONFIG"成"true"
         %TIMEOUT_Print_Command%
     )
 )
@@ -162,7 +162,7 @@ if "%USE_CONFIG%"=="true" (
     if "%NO_PROMPT%" == "false" (
         set NO_PROMPT = true
         if "%DEBUG%"=="true" (
-            echo "USE_CONFIG"�����ó�"true"��"NO_PROMPT"Ӧ�ñ����ó�"true"
+            echo "USE_CONFIG"已设置成"true"，"NO_PROMPT"应该被设置成"true"
             %TIMEOUT_Print_Command%
         )
     )
@@ -172,10 +172,10 @@ if "%USE_CONFIG%"=="true" (
 :input_loop
 if "%USE_CONFIG%"=="true" (
     if "%DEBUG%"=="true" (
-        echo ʹ�������ļ��е��˺����롣
-        echo ��Ҫȡ��������ʾ���ý��棬�뽫�����ļ��е� "NO_PROMPT" ��Ϊ "false"��
-        echo ��Ҫ�ر�Ĭ��ʹ�������ļ����˺����룬�뽫�����ļ��е� "USE_CONFIG" ��Ϊ "false"��
-        echo �����ļ�·��: %CONFIG_FILE%
+        echo 使用配置文件中的账号密码。
+        echo 若要取消不再显示设置界面，请将配置文件中的 "NO_PROMPT" 改为 "false"。
+        echo 若要关闭默认使用配置文件的账号密码，请将配置文件中的 "USE_CONFIG" 改为 "false"。
+        echo 配置文件路径: %CONFIG_FILE%
         %TIMEOUT_Print_Command%
     )
     set USERNAME=%USERNAME%
@@ -183,18 +183,18 @@ if "%USE_CONFIG%"=="true" (
     goto execute_java
 ) else  (
     if "%DEBUG%"=="true" (
-        echo ��ʼ�����˺�����...
+        echo 开始输入账号密码...
     )
 
 
 
-:: ��ʼ������
+:: 初始化变量
 set NEW_USERNAME=""
 set NEW_PASSWORD=""
 
 if not "%USERNAME%" == "" (
     if not "%USERNAME%" == """" (
-    echo ���ϴ�ʹ�õ��˺�Ϊ: %USERNAME%
+    echo 你上次使用的账号为: %USERNAME%
     %TIMEOUT_Print_Command%
     ) else (
         goto Account_Input
@@ -203,47 +203,47 @@ if not "%USERNAME%" == "" (
     goto Account_Input
 )
 
-choice /t %TIMEOUT% /c YN /d Y /m "�Ƿ�ʹ���ϴ��˺� "
+choice /t %TIMEOUT% /c YN /d Y /m "是否使用上次账号 "
 if errorlevel 2 (
     if "%DEBUG%"=="true" (
-        echo �û�ѡ��ʹ��֮ǰ���˺ţ���ת�������˺Ų���
+        echo 用户选择不使用之前的账号，跳转至输入账号部分
     )
     goto Account_Input
 ) else (
     if "%DEBUG%"=="true" (
-        echo �û�ѡ��ʹ��֮ǰ���˺ţ���ת�������䲿��
+        echo 用户选择使用之前的账号，跳转至密码输部分
     )
     goto Password_Input
 )
 
 :Account_Input
-set /p "NEW_USERNAME=�������˺�: "
+set /p "NEW_USERNAME=请输入账号: "
 if "%DEBUG%"=="true" (
-    echo �������ˣ�%NEW_USERNAME%
+    echo 你输入了：%NEW_USERNAME%
 )
 
 if "%NEW_USERNAME%"=="""" (
 		if not "USERNAME"  == "" (
 			if not "USERNAME"  == "" (
-				echo ��������Ϊ�գ���ʹ���ϴ�ʹ�õ��˺�: %USERNAME%
+				echo 输入内容为空，将使用上次使用的账号: %USERNAME%
 				goto Password_Input
 			) else (
-				echo û���˺ţ��ű��޷�����ִ�С�
+				echo 没有账号，脚本无法继续执行。
                 %TIMEOUT_Print_Command%
 				goto END
 			)
 		) else (
-			echo û���˺ţ��ű��޷�����ִ�С�
+			echo 没有账号，脚本无法继续执行。
             %TIMEOUT_Print_Command%
 			goto END
 		)
 	) else (
-		:: �ж�����ĵ�һ�������һ���ַ��Ƿ�ͬʱΪ˫���ţ�����Ӧ��ȥ��
+		:: 判断密码的第一个和最后一个字符是否同时为双引号，并相应地去除
 		:: if "%NEW_USERNAME:~0,1%"=="""" if "%NEW_USERNAME:~-1%"=="""" (
 		:: 	set "USERNAME=!NEW_USERNAME:~1,-1!"
 		:: )
 		
-		:: �ж�����ĵ�һ�������һ���ַ��Ƿ�ͬʱΪ�����ţ�����Ӧ��ȥ��
+		:: 判断密码的第一个和最后一个字符是否同时为单引号，并相应地去除
 		:: if "%NEW_USERNAME:~0,1%"=="'" if "%NEW_USERNAME:~-1%"=="'" (
 		:: 	set "USERNAME=!NEW_USERNAME:~1,-1!"
 		:: )
@@ -256,19 +256,19 @@ if "%NEW_USERNAME%"=="""" (
 	)
 
 :Show_Username
-echo �û��������µ��˺�: %USERNAME%
+echo 用户输入了新的账号: %USERNAME%
 
 
 :Password_Input
 if "%DEBUG%"=="true" (
-	echo �û������벿�ֽ���
-	echo �����û���Ϊ��%USERNAME%
+	echo 用户名输入部分结束
+	echo 最终用户名为：%USERNAME%
 	)
 
 if not "%PASSWORD%" == "" (
     if not "%PASSWORD%" == """" (
         if "%DEBUG%"=="true" (
-            echo ���ϴ�ʹ�õ�����Ϊ: %PASSWORD%
+            echo 你上次使用的密码为: %PASSWORD%
             )
     ) else (
         goto Password_Input_Two
@@ -277,23 +277,23 @@ if not "%PASSWORD%" == "" (
     goto Password_Input_Two
 )
 
-choice /t %TIMEOUT% /c YN /d Y /m "�Ƿ�ʹ���ϴ����� "
+choice /t %TIMEOUT% /c YN /d Y /m "是否使用上次密码 "
 if errorlevel 2 (
     if "%DEBUG%"=="true" (
-        echo �û�ѡ��ʹ��֮ǰ�����룬��ת���������벿�֡�
+        echo 用户选择不使用之前的密码，跳转至输入密码部分。
     )
     goto Password_Input_Two
 ) else (
     if "%DEBUG%"=="true" (
-        echo �û�ѡ��ʹ��֮ǰ������
+        echo 用户选择使用之前的密码
     )
     goto Settings_Page
 )
 
 :Password_Input_Two
-set /p "NEW_PASSWORD=����������: "
+set /p "NEW_PASSWORD=请输入密码: "
 if "%DEBUG%"=="true" (
-    echo �������ˣ�%NEW_PASSWORD%
+    echo 你输入了：%NEW_PASSWORD%
 )
 
 
@@ -301,30 +301,30 @@ if "%DEBUG%"=="true" (
 if "%NEW_PASSWORD%"=="""" (
 		if not "PASSWORD"  == "" (
 			if not "PASSWORD"  == "" (
-				echo ��������Ϊ�գ���ʹ���ϴ�ʹ�õ����롣
+				echo 输入内容为空，将使用上次使用的密码。
 				if not "%PASSWORD%" == "" (
 					if "%DEBUG%"=="true" (
-						echo ���ϴ�ʹ�õ�����Ϊ: %PASSWORD%
+						echo 你上次使用的密码为: %PASSWORD%
 					)
 				)
 				goto Settings_Page
 			) else (
-				echo û�����룬�ű��޷�����ִ�С�
+				echo 没有密码，脚本无法继续执行。
                 %TIMEOUT_Print_Command%
 				goto END
 			)
 		) else (
-			echo û�����룬�ű��޷�����ִ�С�
+			echo 没有密码，脚本无法继续执行。
             %TIMEOUT_Print_Command%
 			goto END
 		)
 	) else (
-		:: �ж�����ĵ�һ�������һ���ַ��Ƿ�ͬʱΪ˫���ţ�����Ӧ��ȥ��
+		:: 判断密码的第一个和最后一个字符是否同时为双引号，并相应地去除
 		:: if "%NEW_PASSWORD:~0,1%"=="""" if "%NEW_PASSWORD:~-1%"=="""" (
 		:: 	set "PASSWORD=!NEW_PASSWORD:~1,-1!"
 		:: )
 		
-		:: �ж�����ĵ�һ�������һ���ַ��Ƿ�ͬʱΪ�����ţ�����Ӧ��ȥ��
+		:: 判断密码的第一个和最后一个字符是否同时为单引号，并相应地去除
 		:: if "%NEW_PASSWORD:~0,1%"=="'" if "%NEW_PASSWORD:~-1%"=="'" (
 		:: 	set "PASSWORD=!NEW_PASSWORD:~1,-1!"
 		:: )
@@ -338,33 +338,33 @@ if "%NEW_PASSWORD%"=="""" (
 
 
 :Show_Password
-echo �û��������µ�����: %PASSWORD%
+echo 用户输入了新的密码: %PASSWORD%
 
 
 
 
 :Settings_Page
 	if "%DEBUG%"=="true" (
-		echo �������벿�ֽ���
-		echo ��������Ϊ��%PASSWORD%
+		echo 密码输入部分结束
+		echo 最终密码为：%PASSWORD%
 		)
     if "%NO_PROMPT%" == "true" (
         echo.
-        echo �����ò��ٵ�������ҳ�棬
-        echo ��Ҫȡ��������ʾ���ý��棬�뽫�����ļ��е� "NO_PROMPT" ��Ϊ "false"��
-        echo �����ļ�·��: %CONFIG_FILE%
+        echo 已设置不再弹出设置页面，
+        echo 若要取消不再显示设置界面，请将配置文件中的 "NO_PROMPT" 改为 "false"。
+        echo 配置文件路径: %CONFIG_FILE%
         echo.
         %TIMEOUT_Print_Command%
 		goto NO_PROMPT_Two
     ) else (
-        :: ��ʼ������
+        :: 初始化变量
         set USE_CONFIG_INPUT = ""
         set NO_PROMPT_INPUT = ""
-        :: ������ʾҪ���û��Ƿ�Ĭ��ʹ�������ļ��е��˺�����
+        :: 弹出提示要求用户是否默认使用配置文件中的账号密码
 		if "%DEBUG%"=="true" (
-			echo ��ǰUSE_CONFIG������%USE_CONFIG%
+			echo 当前USE_CONFIG参数：%USE_CONFIG%
 		)
-        choice /t %TIMEOUT% /c YN /d Y /m "�Ƿ�Ĭ��ʹ�������ļ��е��˺�����? (Y/N��Ĭ��Y): "
+        choice /t %TIMEOUT% /c YN /d Y /m "是否默认使用配置文件中的账号密码? (Y/N，默认Y): "
         if errorlevel 2 (
             goto Set_USE_CONFIG_False
         ) else (
@@ -374,30 +374,30 @@ echo �û��������µ�����: %PASSWORD%
         :Set_USE_CONFIG_False
         set USE_CONFIG=false
         if "%DEBUG%" == "true" (
-            echo ��ȡ������Ĭ��ʹ�������ļ��е��˺����롣
+            echo 已取消设置默认使用配置文件中的账号密码。
             %TIMEOUT_Print_Command%
             if "%DEBUG%"=="true" (
-                echo ��ǰUSE_CONFIG������%USE_CONFIG%
+                echo 当前USE_CONFIG参数：%USE_CONFIG%
                 )
 			goto NO_PROMPT_One
         )
 
         :Set_USE_CONFIG_True
         set USE_CONFIG=true
-        echo ������Ĭ��ʹ�������ļ��е��˺����롣
+        echo 已设置默认使用配置文件中的账号密码。
         %TIMEOUT_Print_Command%
         if "%DEBUG%"=="true" (
-            echo ��ǰUSE_CONFIG������%USE_CONFIG%
+            echo 当前USE_CONFIG参数：%USE_CONFIG%
             )
 		goto NO_PROMPT_One
         
 		
 		:NO_PROMPT_One
-        :: ������ʾҪ���û��Ƿ��ٵ��������ý���
+        :: 弹出提示要求用户是否不再弹出此设置界面
 		if "%DEBUG%"=="true" (
-			echo NO_PROMPT��%NO_PROMPT%
+			echo NO_PROMPT：%NO_PROMPT%
 		)
-        choice /t %TIMEOUT% /c YN /d N /m "�Ƿ��ٵ��������ý���? (Y/N��Ĭ��N): "
+        choice /t %TIMEOUT% /c YN /d N /m "是否不再弹出此设置界面? (Y/N，默认N): "
         if errorlevel 2 (
 			goto Set_NO_PROMPT_False
         ) else (
@@ -407,10 +407,10 @@ echo �û��������µ�����: %PASSWORD%
         :Set_NO_PROMPT_False
         set NO_PROMPT=false
         if "%DEBUG%" == "true" (
-            echo ��ȡ�����ò��ٵ��������ý��档
+            echo 已取消设置不再弹出此设置界面。
             %TIMEOUT_Print_Command%
             if "%DEBUG%"=="true" (
-                echo ��ǰNO_PROMPT������%NO_PROMPT%
+                echo 当前NO_PROMPT参数：%NO_PROMPT%
                 )
 			goto NO_PROMPT_Two
         )
@@ -418,59 +418,59 @@ echo �û��������µ�����: %PASSWORD%
         :Set_NO_PROMPT_True
         set NO_PROMPT=true
         if "%DEBUG%" == "true" (
-            echo �����ò��ٵ��������ý��档
+            echo 已设置不再弹出此设置界面。
             %TIMEOUT_Print_Command%
             if "%DEBUG%"=="true" (
-                echo ��ǰNO_PROMPT������%NO_PROMPT%
+                echo 当前NO_PROMPT参数：%NO_PROMPT%
                 )
 			goto NO_PROMPT_Two
         )
 		
 		:NO_PROMPT_Two
-        :: ����ȡ�����˺���������ô��浽config��
+        :: 将获取到的账号密码和设置储存到config中
         if not exist "%CONFIG_FILE%" (
             type nul > "%CONFIG_FILE%"
             if "%DEBUG%"=="true" (
-                echo �����ļ������ڣ��Ѵ�����
+                echo 配置文件不存在，已创建。
                 %TIMEOUT_Print_Command%
             )
         ) else (
             del "%CONFIG_FILE%"
             type nul > "%CONFIG_FILE%"
             if "%DEBUG%"=="true" (
-                echo �����ļ����ڣ�������ؽ���
+                echo 配置文件存在，已清空重建。
                 %TIMEOUT_Print_Command%
             )
         )
 		if "%DEBUG%"=="true" (
-			echo ��ǰUSERNAME������%USERNAME%
+			echo 当前USERNAME参数：%USERNAME%
 		)
 		if "%DEBUG%"=="true" (
-			echo ��ǰPASSWORD������%PASSWORD%
+			echo 当前PASSWORD参数：%PASSWORD%
 		)
 		if "%DEBUG%"=="true" (
-			echo ��ǰUSE_CONFIG������%USE_CONFIG%
+			echo 当前USE_CONFIG参数：%USE_CONFIG%
 		)
 		if "%DEBUG%"=="true" (
-			echo ��ǰNO_PROMPT������%NO_PROMPT%
+			echo 当前NO_PROMPT参数：%NO_PROMPT%
 		)
         echo USERNAME=%USERNAME%> "%CONFIG_FILE%"
         echo PASSWORD=%PASSWORD%>> "%CONFIG_FILE%"
         echo USE_CONFIG=%USE_CONFIG%>> "%CONFIG_FILE%"
         echo NO_PROMPT=%NO_PROMPT%>> "%CONFIG_FILE%"
         if "%DEBUG%"=="true" (
-            echo �˺�����������ѱ��浽�����ļ���
+            echo 账号密码和设置已保存到配置文件。
         )
     )
 
 :execute_java
-:: ִ��Java����
+:: 执行Java程序
 if "%DEBUG%"=="true" (
-    echo ִ��Java���򣬲���Ϊ: "%JAVA_PATH%" -jar "%JAR_FILE%" -u %USERNAME% -p %PASSWORD%
+    echo 执行Java程序，参数为: "%JAVA_PATH%" -jar "%JAR_FILE%" -u %USERNAME% -p %PASSWORD%
 )
 "%JAVA_PATH%" -jar "%JAR_FILE%" -u %USERNAME% -p %PASSWORD%
 if "%DEBUG%"=="true" (
-    echo Java����ִ����ϡ�
+    echo Java程序执行完毕。
 )
 
 :END
